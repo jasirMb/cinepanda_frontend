d267b093dfb364747a5f08641fbdfc5148aa3b60
jasirMb <jasirmb9118@gmail.com>
2026-03-16 11:12:58 +0530

    Refactor TypeScript configuration and update layout component
    Reformatted tsconfig.json for improved readability and added incremental compilation. Updated layout.tsx to replace QueryClientProvider with a custom QueryProvider for better encapsulation of query logic.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { getQueryClient } from "@/lib/query-client";
+import { QueryProvider } from "@/components/providers/QueryProvider";
 
 export const metadata: Metadata = {
   title: "CinePanda Admin",
@@ -14,15 +12,10 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const queryClient = getQueryClient();
-
   return (
     <html lang="en">
       <body>
-        <QueryClientProvider client={queryClient}>
-          {children}
-          <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
+        <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
   );
